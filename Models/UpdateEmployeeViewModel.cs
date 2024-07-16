efbf1e177d78e39433ecb32fd71caca4946d6534
berk cicek <berk.cicek@mf.karaelmas.edu.tr>
2024-07-16 23:08:01 +0300

    ss
    s

---
+++ Models/UpdateEmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Principal;
+
+namespace ASPNETMVCCRUD.Models
+{
+    public class UpdateEmployeeViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+    }
+}
